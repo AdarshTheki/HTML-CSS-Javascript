eea61439fda34df9f2c8304e9229dc1719735b07
Adarsh Verma <adarshverma549@gmail.com>
2023-03-19 10:41:09 +0000

    modify 12_loop.js

--- JavaScript/12_loop.js
+++ JavaScript/12_loop.js
@@ -1,1 +1,15 @@
 // loop
+
+for(let index = 0; index < 5; index++){
+  console.log(`index of value is: ${index}`)
+}
+
+let heros = ['adarsh','Ayush','Ankita','Sarita',"Pralhad"]
+let another = []
+for (let index=0; index<heros.lenght; index++){
+  let another = []
+  const element  =  heros[index];
+  console.log(`Hero is ${element}`);
+  // another.push(element.toUpperCase())
+}
+console.log(another);
