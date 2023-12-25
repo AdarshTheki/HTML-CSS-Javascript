bbd9a70a21bdd99983cad18bc88ad5c47f71b6f6
Adarsh Verma <adarshverma549@gmail.com>
2023-12-25 21:15:11 +0530

    create own custom event

--- JavaScript/25_CustomEvents.js
+++ JavaScript/25_CustomEvents.js
@@ -1,4 +1,4 @@
-// create custom events
+// Create your own event listeners:
 
 // accept two argument : event name and optional object
 const button = document.getElementById('button');
@@ -7,12 +7,12 @@ button.dispatchEvent(new_event);
 
 // Listening for custom event
 button.addEventListener('demo-event', (event) => {
-  console.log(event.detail); // { name : "javascript" }
+    console.log(event.detail); // { name : "javascript" }
 });
 
 // Removing Event Listeners
 function handleEvent(event) {
-  console.log(event.detail); // { name : "javascript" }
+    console.log(event.detail); // { name : "javascript" }
 }
 button.addEventListener('demo-event', handleEvent);
 button.removeEventListener('demo-event', handleEvent);
