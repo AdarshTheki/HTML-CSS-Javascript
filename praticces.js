11d512ae4fc9d53909802ee1e5a4e35816f5555d
Adarsh Verma <adarshverma549@gmail.com>
2024-01-03 12:54:58 +0530

    modify pratices questions

--- praticces.js
+++ praticces.js
@@ -1,14 +1,22 @@
-function outerFun() {
-    let OuterValue = 'i am Outer Value';
-    function innerFunction() {
-        let innerValue = 'Inner Value';
-        console.log(innerValue, OuterValue);
+// Function expression
+const person = {
+    name: 'Adarsh',
+    age: 24,
+    address: {
+        state: 'MH',
+        city: 'nagpur',
+    },
+};
 
-        (function innerOfInnerFun() {
-            let innerInnerValue = 'Inner Inner value';
-            console.log(innerInnerValue, OuterValue);
-        })();
-    }
-    innerFunction();
-}
-outerFun();
+// const shallowCopy = Object.assign({}, Person);
+const shallowCopy = { ...person };
+
+// const deepCopy = structuredClone(Person);
+const deepCopy = JSON.parse(JSON.stringify(person));
+
+deepCopy.address.state = 'UP';
+deepCopy.address.city = 'Kanpur';
+deepCopy.age = 100;
+deepCopy.name = 'Ankita';
+
+console.log(person);
