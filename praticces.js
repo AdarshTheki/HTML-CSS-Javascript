8b2babf4a089013e41273961bfcf07c155e124c7
Adarsh Verma <adarshverma549@gmail.com>
2024-01-09 21:54:16 +0530

    modify the final commit

--- praticces.js
+++ praticces.js
@@ -1,22 +1,4 @@
-// Function expression
-const person = {
-    name: 'Adarsh',
-    age: 24,
-    address: {
-        state: 'MH',
-        city: 'nagpur',
-    },
-};
+const data =
 
-// const shallowCopy = Object.assign({}, Person);
-const shallowCopy = { ...person };
-
-// const deepCopy = structuredClone(Person);
-const deepCopy = JSON.parse(JSON.stringify(person));
-
-deepCopy.address.state = 'UP';
-deepCopy.address.city = 'Kanpur';
-deepCopy.age = 100;
-deepCopy.name = 'Ankita';
-
-console.log(person);
+const data2 = data.splice(0, 2);
+console.log(data, data2);
